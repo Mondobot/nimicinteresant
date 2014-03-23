b1bfb9df5e0d0722936d7317f45b49777a344067
mondobot <mateipavaluca@yahoo.com>
2014-03-23 11:55:53 +0200

    Implemented User.java class

--- src/model/User.java
+++ src/model/User.java
@@ -1,5 +1,32 @@
 package model;
 
 public class User {
-
+	String name;
+	Integer ID;
+	
+	public User() {
+		this.name = "None";
+		this.ID = -1;
+	}
+	
+	public User(String name, Integer ID) {
+		this.name = name;
+		this.ID = ID;
+	}
+	
+	String getName() {
+		return this.name;
+	}
+	
+	void setName(String newName) {
+		this.name = newName;
+	}
+	
+	Integer getID() {
+		return this.ID;
+	}
+	
+	void setID(Integer newID) {
+		this.ID = newID;
+	}
 }
