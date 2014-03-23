ecece826414245082860bafc26b295d4130d2a26
mondobot <mateipavaluca@yahoo.com>
2014-03-23 14:08:23 +0200

    Added Controller code

--- src/controller/Controller.java
+++ src/controller/Controller.java
@@ -1,5 +1,12 @@
 package controller;
 
+import java.util.ArrayList;
+
+import javax.print.attribute.standard.Media;
+
+import mediator.Mediator;
+import model.*;
+
 public class Controller {
 	private static Controller con;
 	
@@ -11,5 +18,22 @@ public class Controller {
 			con = new Controller();
 		
 		return con;
+	}
+	
+	public void updateUsers() {
+		ArrayList<User> newUsers = Mediator.getInstance().getUsers();
+		Model.getInstance().setUsers(newUsers);
+	}
+	
+	public void updateFiles() {
+		Integer myID = Model.getInstance().getMyUser().getID();
+		ArrayList<File> newFiles = Mediator.getInstance().getFiles(myID);
+		Model.getInstance().setFiles(newFiles);
+	}
+	
+	public void updateTransfers() {
+		Integer myID = Model.getInstance().getMyUser().getID();
+		ArrayList<Transfer> newTrans = Mediator.getInstance().getTransfers(myID);
+		Model.getInstance().setTransfers(newTrans);
 	}
 }
